2bae4e420731069b2cad624195576fd7d43ac57d
Kushul Soomaree <kushul@lsl.digital>
2022-06-05 21:08:53 +0400

    dist folder

---
+++ app/dist/precache-manifest.8a992e7e44dddd51978964f6a77e6607.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef2f48d8b4f9b2d4a10b",
+    "url": "/css/app.8d23bc70.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "0f9f33d4f55dc570a0247d8685c9f58c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da783575d315a3323711",
+    "url": "/js/about.60621270.js"
+  },
+  {
+    "revision": "ef2f48d8b4f9b2d4a10b",
+    "url": "/js/app.3cd9e953.js"
+  },
+  {
+    "revision": "b82ee8573ee3e0f59297",
+    "url": "/js/chunk-vendors.24adfeac.js"
+  },
+  {
+    "revision": "833d8182528a24c31b14f9eb0af10af2",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
